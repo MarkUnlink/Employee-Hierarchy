b35e10852a759235ebea2225304724a50714d83e
MarkUnlink <31216935+MarkUnlink@users.noreply.github.com>
2019-09-15 11:15:57 +0300

    Completed the primary code

--- Employee Hierarchy/Employees.cs
+++ Employee Hierarchy/Employees.cs
@@ -1,25 +1,22 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Employee_Hierarchy
 {
-    public class Employees
+    class Employees
     {
+        public List<string> emps = new List<string>();
+        public List<KeyValuePair<string, string>> employees = new List<KeyValuePair<string, string>>();
+        public List<KeyValuePair<string, int>> salaries = new List<KeyValuePair<string, int>>();
 
-        private Tree<Emp> empTree;
-        private Dictionary<string, Emp> employee;
-
-        public Employees(string[] employeesCSV)
+        public Employees(string[] employeeCSV)
         {
-            int salary = 0, CEO = 0;
-
-            empTree = new Tree<Emp>();
-            employee = new Dictionary<string, Emp>();
-
-            var lines = employeesCSV.Select(items => items.Split('\t'));
+            
+            var lines = employeeCSV.Select(items => items.Split('\n'));
             var section = from line in lines select (from value in line select value);
 
+            int CEO = 0, num=0;
 
             foreach (var i in section)
             {
@@ -30,98 +27,120 @@ namespace Employee_Hierarchy
                     try
                     {
                         var item = x.Current.Split(',');
+                    //copies a record of all employees to list
+                        emps.Add(item[0]);
 
-                 // This code ensures that the salary in the CSV is a valid integer
-                        if (int.TryParse(item[2], out salary))
+                    // THis code identifies only 1 CEO and validates that there isn't another null entry
+                        if (string.IsNullOrEmpty(item[0]))
                         {
-                            var emp = new Emp(item[0], item[1], salary);
+                            Console.WriteLine("ERROR: Employee cannot be empty !");
+                            break;
+                        }
+                        else if (string.IsNullOrEmpty(item[1]) && CEO == 1)
+                        {
+                            Console.WriteLine("ERROR: There can only be 1 CEO !");
+                            break;
+                        }
+
+                    // This code verifies that the salaries are only integers
+
+                        if (int.TryParse(item[2], out num))
+                        {
                             try
                             {
-                                employee.Add(emp.Id, emp);
+                                salaries.Add(new KeyValuePair<string, int>(item[1], int.Parse(item[2])));
                             }
                             catch (Exception e)
                             {
                                 Console.WriteLine(e);
                             }
-
-                 // THis code adds the employee to the DAG if not present
-                            if (!empTree.HasVertex(emp))
-                            {
-                                empTree.AddVertex(emp);
-                            }
                         }
                         else
                         {
-                            Console.WriteLine("ERROR: Salary is NOT an integer!");
+                            Console.WriteLine("ERROR: Salaries must be Integers !");
+                            break;
                         }
 
-                 // THis code 
-                        if (string.IsNullOrEmpty(item[0]))
+                        foreach (var keyValue in employees)
                         {
-                            Console.WriteLine("Employee cannot have empty Id, skipping ...");
-                            continue;
+                            string k = keyValue.Key;
+                            string v = keyValue.Value;
+
+                        /* THis code ensures that the employee does NOT report to more than 1 manager,
+                         * by ensuring the same employee doesn't appear more than once.
+                        */
+
+                            if (k == item[0])
+                            {
+                                Console.WriteLine("ERROR: Employee already has a manager/ Exists !");
+                                break;
+                            }
+
+                        // This code checks for circular reference and stops
+                            if (item[0]== v)
+                            {
+                                foreach (var kv in employees)
+                                {
+                                    string a = kv.Key;
+                                    string b = kv.Value;
+
+                                    if (k == b)
+                                    {
+                                        if (item[1]==a)
+                                        {
+                                            Console.WriteLine("ERROR: Circular reference not allowed");
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+
                         }
+
+                    // This checks if the Manager is also an employee
+                        if (emps.Contains(item[1]))
+                            {
+                                employees.Add(new KeyValuePair<string, string>(item[0], item[1]));
+                            }
 
                         if (string.IsNullOrEmpty(item[1]) && CEO < 1)
+                            {
+                            
+                                employees.Add(new KeyValuePair<string, string>(item[0], null));
+                                CEO++;
+                            }
+
+                        else
                         {
-                            CEO++;
-                        }
-                        else if (string.IsNullOrEmpty(item[1]) && CEO == 1)
-                        {
-                            Console.WriteLine("There can only be 1 ceo in the organization, skipping ...");
-                            continue;
+                            Console.WriteLine("ERROR: Manager MUST be an employee !");
+                            break;
                         }
 
                     }
+
                     catch (Exception e)
                     {
                         Console.WriteLine(e);
                     }
                 }
-                x.Dispose();
-
             }
-
-            foreach (KeyValuePair<string, Emp> kvp in employee)
-            {
-                if (!string.IsNullOrEmpty(kvp.Value.Manager))
-                {
-                // This code checks for circular referencing
-                    bool doubleLinked = false;
-                    foreach (Emp employee in empTree.DepthFirstWalk(kvp.Value).ToArray())
-                    {
-                        if (employee.Equals(kvp.Value.Manager))
-                        {
-                            doubleLinked = true;
-                            break;
-                        }
-                    }
-                // THis code ensures that employees have only one manager
-                    if (empTree.IncomingEdges(kvp.Value).ToArray().Length < 1 && !doubleLinked)
-                    {
-                        empTree.AddEdge(employee[kvp.Value.Manager], kvp.Value);
-                    }
-                    else
-                    {
-                        Console.WriteLine("ERROR: Double linking not allowed");
-                    }
-                }
-
-            }
-
         }
 
-    // THis Code returns the salary budget for the manager
-        public long Budget(string sal)
+    // THis method adds salaries from the specified manager
+        public long Budget(string manager)
         {
             var budget = 0;
             try
             {
-                var employeesInPath = empTree.DepthFirstWalk(employee[sal]).GetEnumerator();
-                while (employeesInPath.MoveNext())
+                foreach (var sal in salaries)
                 {
-                    budget += employeesInPath.Current.Salary;
+                    string y = sal.Key;
+                    int z = sal.Value;
 
+                    if (manager == y)
+                    {
+                        budget += z;
+                    }
                 }
             }
             catch (Exception e)
@@ -131,6 +150,6 @@ namespace Employee_Hierarchy
 
             return budget;
         }
+
     }
- 
 }
